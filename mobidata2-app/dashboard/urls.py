be343fdf44446afc06e135cd46fe60104c63ea0e
Lucas Vinicius <lucasviniciuslopez@gmail.com>
2025-06-29 22:51:47 -0300

    adiciona novo gráfico de teste

--- mobidata2-app/dashboard/urls.py
+++ mobidata2-app/dashboard/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('matplot', views.testematplotlib, name='testematplotlib'),
-    path('matplot2', views.testematplotlib2, name='testematplotlib2'),
-    path('historico_idade_media_frota', views.historico_idade_media_frota, name='historico_idade_media_frota'),
+    path('', views.mainpage_dashboard, name='mainpage_dashboard'),
     path('exportar_csv', views.exportar_csv, name='exportar_csv'),
 ]
--- mobidata2-app/dashboard/views.py
+++ mobidata2-app/dashboard/views.py
@@ -3,82 +3,42 @@ from django.template import loader
 from django.http import HttpResponse
 
 from .models import HistoricoIdadeMediaFrota
-
-import io
-import base64
-import matplotlib.pyplot as matplot
+from .models import FrotaTotal
 
 import pandas
 import plotly.express as plotxp
 
-def home(request):
-    template = loader.get_template('homepage.html')
-    return HttpResponse(template.render())
-
-def testematplotlib(request):
-    dados = HistoricoIdadeMediaFrota.objects.filter(ano=2022).order_by('mes')
-
-    meses = [d.mes for d in dados]
-    idade_media = [d.idade_media for d in dados]
-
-    matplot.figure(figsize=(8,4))
-    matplot.plot(meses, idade_media, marker='o', color='blue')
-    matplot.title('Idade Média da Frota - 2022')
-    matplot.xlabel('Mês')
-    matplot.ylabel('Idade Média (anos)')
-    matplot.xticks(meses)
-    matplot.grid(True)
-
-    buf = io.BytesIO()
-    matplot.savefig(buf, format='png')
-    matplot.close()
-    buf.seek(0)
-    image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
-    buf.close()
-
-    return render(request, 'testematplotlib.html', { 'grafico': image_base64 })
-
-def testematplotlib2(request):
-    dados = HistoricoIdadeMediaFrota.objects.all().order_by('ano', 'mes')
-
-    x_labels = [f"{d.ano}-{d.mes:02d}" for d in dados]
-    idade_media = [d.idade_media for d in dados]
-
-    matplot.figure(figsize=(58,12))
-    matplot.plot(x_labels, idade_media, marker='o', color='blue')
-    matplot.title('Histórico Completo da Idade Média da Frota')
-    matplot.xlabel('Ano e Mês')
-    matplot.ylabel('Idade Média (anos)')
-    matplot.xticks(rotation=90, fontsize=8)
-    matplot.grid(True)
-
-    buf = io.BytesIO()
-    matplot.savefig(buf, format='png', bbox_inches='tight')
-    matplot.close()
-    buf.seek(0)
-    grafico_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
-    buf.close()
-
-    return render(request, 'testematplotlib2.html', { 'grafico': grafico_base64 })
-
-def historico_idade_media_frota(request):
+def mainpage_dashboard(request):
     anos_disponiveis = HistoricoIdadeMediaFrota.objects.values_list('ano', flat=True).distinct().order_by('ano')
-    ano_selecionado = int(request.GET.get('ano', anos_disponiveis.first()))
+    ano_selecionado = int(request.GET.get('ano', 2022))
+
+    ###################### HISTORICO DE IDADE MEDIA
 
     historico_filtrado = HistoricoIdadeMediaFrota.objects.filter(ano=ano_selecionado).order_by('mes')
 
     historico_dataframe = pandas.DataFrame(list(historico_filtrado.values('ano', 'mes', 'idade_media')))
     historico_dataframe['mes'] = historico_dataframe['mes'].apply(lambda x: f"{x:02d}")
 
-    grafico = plotxp.line(historico_dataframe, x='mes', y='idade_media', markers=True, title=f'Idade Média da Frota - {ano_selecionado}')
-    grafico.update_layout(xaxis_title='Mês', yaxis_title='Idade Média (anos)')
+    grafico_historico = plotxp.line(historico_dataframe, x='mes', y='idade_media', markers=True, title=f'Idade Média da Frota - {ano_selecionado}')
+    grafico_historico.update_layout(xaxis_title='Mês', yaxis_title='Idade Média (anos)')
 
-    grafico_html = grafico.to_html(full_html=False)
+    ###################### FROTA TOTAL
 
-    return render(request, 'historico_idade_media_frota.html', {
-        'grafico': grafico_html,
+    frota_total = FrotaTotal.objects.filter(ano=ano_selecionado).order_by('mes')
+
+    frota_total_dataframe = pandas.DataFrame(list(frota_total.values('ano', 'mes', 'id_concessionaria','qtd_total_de_onibus')))
+    frota_total_dataframe['mes'] = frota_total_dataframe['mes'].apply(lambda x: f"{x:02d}")
+
+    grafico_frota_total = plotxp.bar(frota_total_dataframe, x='mes', y='qtd_total_de_onibus', title='Frota Total de Ônibus')
+    grafico_frota_total.update_layout(xaxis_title='Mês', yaxis_title='Quantidade Total')
+
+    ###################### RENDER
+
+    return render(request, 'mainpage_dashboard.html', {
         'anos': anos_disponiveis,
-        'ano_selecionado': ano_selecionado
+        'ano_selecionado': ano_selecionado,
+        'grafico_historico': grafico_historico.to_html(full_html=False),
+        'grafico_frota_total': grafico_frota_total.to_html(full_html=False),
     })
 
 def exportar_csv(request):
