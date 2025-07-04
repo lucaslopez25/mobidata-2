c68d9bdcc0028d265b0bbfd683802ca4cd7944fd
Lucas Vinicius <lucasviniciuslopez@gmail.com>
2025-07-04 12:02:01 -0300

    melhora questão dos gráficos quebrando páginas quando dados não existem no banco

--- mobidata2-app/dashboard/views.py
+++ mobidata2-app/dashboard/views.py
@@ -19,29 +19,37 @@ def frota_de_onibus_por_ano(request):
 
     historico_filtrado = HistoricoIdadeMediaFrota.objects.filter(ano=ano_selecionado).order_by('mes')
 
-    historico_dataframe = pandas.DataFrame(list(historico_filtrado.values('ano', 'mes', 'idade_media')))
-    historico_dataframe['mes'] = historico_dataframe['mes'].apply(lambda x: f"{x:02d}")
+    if historico_filtrado.exists():
+        historico_dataframe = pandas.DataFrame(list(historico_filtrado.values('ano', 'mes', 'idade_media')))
+        historico_dataframe['mes'] = historico_dataframe['mes'].apply(lambda x: f"{x:02d}")
 
-    grafico_historico = plotxp.line(historico_dataframe, x='mes', y='idade_media', markers=True, title=f'Idade Média da Frota - {ano_selecionado}')
-    grafico_historico.update_layout(xaxis_title='Mês', yaxis_title='Idade Média (anos)')
+        grafico_historico = plotxp.line(historico_dataframe, x='mes', y='idade_media', markers=True, title=f'Idade Média da Frota - {ano_selecionado}')
+        grafico_historico.update_layout(xaxis_title='Mês', yaxis_title='Idade Média (anos)')
+        grafico_historico = grafico_historico.to_html(full_html=False)
+    else:
+        grafico_historico = None
 
     ###################### FROTA TOTAL
 
     frota_total = FrotaTotal.objects.filter(ano=ano_selecionado).order_by('mes')
+    
+    if frota_total.exists():
+        frota_total_dataframe = pandas.DataFrame(list(frota_total.values('ano', 'mes', 'id_concessionaria','qtd_total_de_onibus')))
+        frota_total_dataframe['mes'] = frota_total_dataframe['mes'].apply(lambda x: f"{x:02d}")
 
-    frota_total_dataframe = pandas.DataFrame(list(frota_total.values('ano', 'mes', 'id_concessionaria','qtd_total_de_onibus')))
-    frota_total_dataframe['mes'] = frota_total_dataframe['mes'].apply(lambda x: f"{x:02d}")
-
-    grafico_frota_total = plotxp.bar(frota_total_dataframe, x='mes', y='qtd_total_de_onibus', title='Frota Total de Ônibus')
-    grafico_frota_total.update_layout(xaxis_title='Mês', yaxis_title='Quantidade Total')
+        grafico_frota_total = plotxp.bar(frota_total_dataframe, x='mes', y='qtd_total_de_onibus', title='Frota Total de Ônibus')
+        grafico_frota_total.update_layout(xaxis_title='Mês', yaxis_title='Quantidade Total')
+        grafico_frota_total = grafico_frota_total.to_html(full_html=False)
+    else:
+        grafico_frota_total = None
 
     ###################### RENDER
 
     return render(request, 'dash-onibus.html', {
         'anos': anos_disponiveis,
         'ano_selecionado': ano_selecionado,
-        'grafico_historico': grafico_historico.to_html(full_html=False),
-        'grafico_frota_total': grafico_frota_total.to_html(full_html=False),
+        'grafico_historico': grafico_historico,
+        'grafico_frota_total': grafico_frota_total,
     })
 
 def exportar_csv(request):
