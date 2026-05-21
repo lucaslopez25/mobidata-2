76394d66ebc57a7f75f2da731f89e18a7c6a902c
Lucas Vinicius <lucasviniciuslopez@gmail.com>
2026-05-21 15:38:22 -0300

    adiciona -d postgres no init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -4,7 +4,7 @@ until pg_isready -U "$POSTGRES_USER" -d postgres; do
   sleep 1
 done
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d postgres <<-EOSQL
     CREATE DATABASE dados_mobilidade;
 EOSQL
 
