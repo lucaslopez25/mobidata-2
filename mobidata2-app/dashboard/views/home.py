fad4855449c82a3deb0c58aa91cd4be436f074eb
Lucas Vinicius <lucasviniciuslopez@gmail.com>
2026-09-20 22:32:33 -0300

    Troca include_plotlyjs='cdn' para include_plotlyjs=False em todas as ocasiões - PlotlyJS já está presente via CDN no head de home.html

--- mobidata2-app/dashboard/views/home.py
+++ mobidata2-app/dashboard/views/home.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
-from dashboard.charts_and_data import climatizacao_frota_total, climatizacao_frota_por_consorcio, assaltos_por_viagem, metro_pax_transportados_por_ano, metro_pax_transportados_por_mes, aquisicoes_onibus_novos_por_ano, aquisicoes_onibus_novos_por_mes, aquisicoes_onibus_usados_por_mes, historico_idade_media_da_frota, comparativo_frota_operante_frota_total, frota_operante, frota_total, historico_linhas_regulares, comparativo_corredores_de_onibus, demanda_pax_stec, demanda_pax_pagamento, demanda_pax_integracao, historico_pax_transportados, historico_km_percorridos, receitas_por_concessionaria
+from dashboard.charts_and_data import (
+    climatizacao_frota_total,
+    climatizacao_frota_por_consorcio,
+    assaltos_por_viagem,
+    metro_pax_transportados_por_ano,
+    metro_pax_transportados_por_mes,
+    aquisicoes_onibus_novos_por_ano,
+    aquisicoes_onibus_novos_por_mes,
+    aquisicoes_onibus_usados_por_mes,
+    historico_idade_media_da_frota,
+    comparativo_frota_operante_frota_total,
+    frota_operante,
+    frota_total,
+    historico_linhas_regulares,
+    comparativo_corredores_de_onibus,
+    demanda_pax_stec,
+    demanda_pax_pagamento,
+    demanda_pax_integracao,
+    historico_pax_transportados,
+    historico_km_percorridos,
+    receitas_por_concessionaria
+)
 
 def home(request):
 
@@ -8,50 +29,50 @@ def home(request):
         ### Ônibus Municipal (STCO)
         # Idade e Renovação da Frota
         'grafico_historico_idade_media_da_frota': 
-            historico_idade_media_da_frota.get_historico_idade_media_da_frota().to_html(full_html=False, include_plotlyjs='cdn'),
+            historico_idade_media_da_frota.get_historico_idade_media_da_frota().to_html(full_html=False, include_plotlyjs=False),
         'grafico_aquisicoes_onibus_novos_por_ano': 
-            aquisicoes_onibus_novos_por_ano.get_grafico_aquisicoes_onibus_novos_por_ano().to_html(full_html=False, include_plotlyjs='cdn'),
+            aquisicoes_onibus_novos_por_ano.get_grafico_aquisicoes_onibus_novos_por_ano().to_html(full_html=False, include_plotlyjs=False),
         'grafico_aquisicoes_onibus_novos_por_mes': 
-            aquisicoes_onibus_novos_por_mes.get_aquisicoes_onibus_novos_por_mes().to_html(full_html=False, include_plotlyjs='cdn'),
+            aquisicoes_onibus_novos_por_mes.get_aquisicoes_onibus_novos_por_mes().to_html(full_html=False, include_plotlyjs=False),
         'grafico_aquisicoes_onibus_usados_por_mes': 
-            aquisicoes_onibus_usados_por_mes.get_aquisicoes_onibus_usados_por_mes().to_html(full_html=False, include_plotlyjs='cdn'),
+            aquisicoes_onibus_usados_por_mes.get_aquisicoes_onibus_usados_por_mes().to_html(full_html=False, include_plotlyjs=False),
         
         # Tamanho da Frota
-        'grafico_comparativo_frota_operante_frota_total': comparativo_frota_operante_frota_total.get_comparativo_frota_operante_frota_total().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_frota_operante': frota_operante.get_frota_operante().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_frota_total': frota_total.get_frota_total().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_comparativo_frota_operante_frota_total': comparativo_frota_operante_frota_total.get_comparativo_frota_operante_frota_total().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_frota_operante': frota_operante.get_frota_operante().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_frota_total': frota_total.get_frota_total().to_html(full_html=False, include_plotlyjs=False),
 
         # Oferta de Linhas e Ônibus
-        'grafico_historico_linhas_regulares': historico_linhas_regulares.get_historico_linhas_regulares().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_comparativo_corredores_de_onibus': comparativo_corredores_de_onibus.get_comparativo_corredores_de_onibus().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_historico_linhas_regulares': historico_linhas_regulares.get_historico_linhas_regulares().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_comparativo_corredores_de_onibus': comparativo_corredores_de_onibus.get_comparativo_corredores_de_onibus().to_html(full_html=False, include_plotlyjs=False),
 
         # Passageiros
-        'grafico_historico_pax_transportados': historico_pax_transportados.get_historico_pax_transportados().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_demanda_pax_pagamento': demanda_pax_pagamento.get_demanda_pax_pagamento().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_demanda_pax_integracao': demanda_pax_integracao.get_demanda_pax_integracao().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_historico_pax_transportados': historico_pax_transportados.get_historico_pax_transportados().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_demanda_pax_pagamento': demanda_pax_pagamento.get_demanda_pax_pagamento().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_demanda_pax_integracao': demanda_pax_integracao.get_demanda_pax_integracao().to_html(full_html=False, include_plotlyjs=False),
 
         # Oferta de Viagens e Quilometragem
-        'grafico_historico_km_percorridos': historico_km_percorridos.get_historico_km_percorridos().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_historico_km_percorridos': historico_km_percorridos.get_historico_km_percorridos().to_html(full_html=False, include_plotlyjs=False),
 
         # Climatização da Frota
-        'grafico_climatizacao_frota_total': climatizacao_frota_total.get_climatizacao_frota_total().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_climatizacao_frota_por_consorcio': climatizacao_frota_por_consorcio.get_climatizacao_frota_por_consorcio().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_climatizacao_frota_total': climatizacao_frota_total.get_climatizacao_frota_total().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_climatizacao_frota_por_consorcio': climatizacao_frota_por_consorcio.get_climatizacao_frota_por_consorcio().to_html(full_html=False, include_plotlyjs=False),
 
         # Receitas e Custos
         'grafico_receitas_por_concessionaria': receitas_por_concessionaria.get_receitas_por_concessionaria(),
 
         ### Metrô
         # Passageiros 
-        'grafico_metro_pax_transportados_por_ano': metro_pax_transportados_por_ano.get_metro_pax_transportados_por_ano().to_html(full_html=False, include_plotlyjs='cdn'),
-        'grafico_metro_pax_transportados_por_mes': metro_pax_transportados_por_mes.get_metro_pax_transportados_por_mes().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_metro_pax_transportados_por_ano': metro_pax_transportados_por_ano.get_metro_pax_transportados_por_ano().to_html(full_html=False, include_plotlyjs=False),
+        'grafico_metro_pax_transportados_por_mes': metro_pax_transportados_por_mes.get_metro_pax_transportados_por_mes().to_html(full_html=False, include_plotlyjs=False),
 
         ### STEC
         # Passageiros
-        'grafico_demanda_pax_stec': demanda_pax_stec.get_demanda_pax_stec().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_demanda_pax_stec': demanda_pax_stec.get_demanda_pax_stec().to_html(full_html=False, include_plotlyjs=False),
 
         ### Indicadores
         # Segurança Pública e Mobilidade
-        'grafico_assaltos_por_viagem': assaltos_por_viagem.get_assaltos_por_viagem().to_html(full_html=False, include_plotlyjs='cdn'),
+        'grafico_assaltos_por_viagem': assaltos_por_viagem.get_assaltos_por_viagem().to_html(full_html=False, include_plotlyjs=False),
     }
 
     return render(request, 'home.html', context)
